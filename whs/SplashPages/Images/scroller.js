a1e3d64a850b03db09c5d2dcbc919e6f0ce61ba3
Benjamin Gwynn <xenxier@gmail.com>
2015-03-17 23:48:38 +0000

    oops

---
+++ whs/SplashPages/Images/scroller.js
@@ -0,0 +1,18 @@
+sts_bs("jwscroller2dbd",[20080623,"http://www.whitchurchhs.com/homepage_Images/images/","","blank.gif",1,1,1,5,"960px","center",0,0,960,433,1,433,0,0,0,4000,1,24,2,"stEffect(\"Active(StartOpc=0,StopOpc=100,Rate=24,enabled=0,Duration=1.00)\")",-2,10],["none",1,"#454545","transparent","","repeat"]);
+sts_ai("i0",[0,"","","","01.jpg",960,433,"center"],["transparent","8pt Verdana,Arial","#999999","none","8pt Verdana,Arial","#999999","none"]);
+sts_ai("i1",[,,,,"02.jpg"],[],"i0","i0");
+sts_ai("i2",[,,,,"03.jpg"],[],"i0","i0");
+sts_ai("i3",[,,,,"04.jpg"],[],"i0","i0");
+sts_ai("i4",[,,,,"05.jpg"],[],"i0","i0");
+sts_ai("i5",[,,,,"06.jpg"],[],"i0","i0");
+sts_ai("i6",[,,,,"07.jpg"],[],"i0","i0");
+sts_ai("i7",[,,,,"08.jpg"],[],"i0","i0");
+sts_ai("i8",[,,,,"09.jpg"],[],"i0","i0");
+sts_ai("i9",[,,,,"10.jpg"],[],"i0","i0");
+sts_ai("i10",[,,,,"11.jpg"],[],"i0","i0");
+sts_ai("i11",[,,,,"12.jpg"],[],"i0","i0");
+sts_ai("i12",[,,,,"13.jpg"],[],"i0","i0");
+sts_ai("i13",[,,,,"14.jpg"],[],"i0","i0");
+sts_ai("i14",[,,,,"15.jpg"],[],"i0","i0");
+sts_ai("i15",[,,,,"16.jpg"],[],"i0","i0");
+sts_es();
