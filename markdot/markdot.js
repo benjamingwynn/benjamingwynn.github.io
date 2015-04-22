1222a3db20106c367da1d707cf50044a66d82de4
Benjamin Gwynn <xenxier@gmail.com>
2015-04-22 04:14:24 +0100

    This should only append the content, and not the last line

--- markdot/markdot.js
+++ markdot/markdot.js
@@ -69,7 +69,7 @@ var markdot = {
 			$(target).append(content[0]);
 			$(target).append(markdot.buildFooter(content[1]));
 		} else {
-			$(target).append(markdot.buildContent(0));
+			$(target).append(markdot.buildContent(0)[0]);
 		}
 		
 		// Log time
